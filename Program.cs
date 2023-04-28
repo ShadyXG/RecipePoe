e3de28829531d7e144f80c414d6651136907bf66
Shady <shadyxgottwald99@gmail.com>
2023-04-28 09:31:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RecipePoe;
+
+internal class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");    
+
+    }
+}    
+
+class RecipeManager {
+    static void Menu() {
+        Console.WriteLine();
+    }
+}    
+
+class Ingredient {   
+
+}
+
+class Recipe {  
+    public string Name {get; set; }   
+
+}
