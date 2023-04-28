be04f32b64fb61d13d5606e7a788627b6233d651
Shady <shadyxgottwald99@gmail.com>
2023-04-28 10:17:28 +0200

    Added an interface for menu methods

--- Program.cs
+++ Program.cs
@@ -7,11 +7,35 @@ internal class Program {
     }
 }    
 
-class RecipeManager {
-    static void Menu() {
-        Console.WriteLine();
+interface RecipeManager {
+
+    void Menu();
+     void AddNewRecipe();
+    void UpscaleQuantities();
+    void ResetValues();
+    void RemoveRecipe();
+}
+class RecipeManagerImpl : RecipeManager {
+    public void AddNewRecipe() {
+        throw new NotImplementedException();
     }
-}    
+
+    public void Menu() {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveRecipe() {
+        throw new NotImplementedException();
+    }
+
+    public void ResetValues() {
+        throw new NotImplementedException();
+    }
+
+    public void UpscaleQuantities() {
+        throw new NotImplementedException();
+    }
+}
 
 class Ingredient {   
 
